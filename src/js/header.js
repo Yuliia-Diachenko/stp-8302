e62bcae0a9d652f1820125321b53ef374ad5c5bc
Yuliia Diachenko <zirkanew82@gmail.com>
2025-03-17 13:38:00 +0200

    fix styles nav

--- src/js/header.js
+++ src/js/header.js
@@ -12,3 +12,35 @@ navLinks.forEach(link => {
     mobileWrapper.classList.remove('open');
   });
 });
+// navigation  smoth
+
+document.addEventListener('DOMContentLoaded', () => {
+  const anchors = document.querySelectorAll('a[href^="#"]');
+  const header = document.querySelector('.header');
+
+  if (header) {
+    const headerHeight = header.offsetHeight + 20;
+    anchors.forEach(anchor => {
+      anchor.addEventListener('click', function (event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        if (targetElement) {
+          const offsetPosition = targetElement.offsetTop - headerHeight;
+
+          window.scrollTo({
+            top: offsetPosition,
+            behavior: 'smooth',
+          });
+
+          // close menu after click
+          if (mobileWrapper.classList.contains('open')) {
+            mobileWrapper.classList.remove('open');
+          }
+        }
+      });
+    });
+  }
+});
